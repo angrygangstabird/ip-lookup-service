01aef12b701e5f2ae85633373fe85d3864f649bd
angrygangstabird <angrygangstabird@gmail.com>
2026-05-17 23:55:05 +0300

    Update src/main/java/com/workspace/iplookup/IpLookupApplication.java

--- src/main/java/com/workspace/iplookup/IpLookupApplication.java
+++ src/main/java/com/workspace/iplookup/IpLookupApplication.java
@@ -3,9 +3,11 @@ package com.workspace.iplookup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 public class IpLookupApplication {
 
     public static void main(String[] args) {
