1e63d48e9e4833b8574f3490653ea5ddd4886351
angrygangstabird <angrygangstabird@gmail.com>
2026-05-17 23:45:09 +0300

    Add src/main/java/com/workspace/iplookup/config/CacheConfig.java

---
+++ src/main/java/com/workspace/iplookup/config/CacheConfig.java
@@ -0,0 +1,28 @@
+package com.workspace.iplookup.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .recordStats();
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("ipLookup");
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+}
