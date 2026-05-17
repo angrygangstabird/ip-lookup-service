ce94e20bda3809b6ef77b76b8d8cde04b6ddbe6c
angrygangstabird <angrygangstabird@gmail.com>
2026-05-17 23:45:10 +0300

    Add src/main/java/com/workspace/iplookup/model/ErrorResponse.java

---
+++ src/main/java/com/workspace/iplookup/model/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.workspace.iplookup.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+}
